9a3a1488eaaf1051cfb3398663415b759fba89ed
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2025-12-17 09:10:39 +0700

    final

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { createClient } from '@/lib/supabase/server'
+import {logoutUser} from '@/app/actions/auth'
 import { redirect } from 'next/navigation'
 
 export default async function DashboardPage() {
@@ -12,7 +13,22 @@ export default async function DashboardPage() {
  return (
  <div className="p-8">
  <h1>Welcome, {user.email}</h1>
- {/* Render notes here */}
- </div>
+
+    {notes?.map((note) => (
+        <div key={note.id} className="max-w-sm rounded overflow-hidden shadow-lg bg-gray-700 my-5">
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{note.title}</div>
+            <p className="text-white text-base">
+            {note.content}
+            </p>
+        </div>
+    </div>))}
+    
+    <form action={logoutUser}>
+    <button className="bg-red-600 text-white px-4 py-2 rounded-lg">
+        Sign Out
+    </button>
+    </form>
+</div>
  )
 }
